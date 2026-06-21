2796350ae8ec70e7d06a9f954c44a05fffe03bdd
feng-qi-yun <zsb1126@outlook.com>
2026-06-21 15:31:57 +0800

    download a tempalate

---
+++ src/client/java/ddnet_mc/client/Ddnet_mcClient.java
@@ -0,0 +1,10 @@
+package ddnet_mc.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Ddnet_mcClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
